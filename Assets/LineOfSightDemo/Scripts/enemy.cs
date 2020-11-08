e92137be472b64ef77c3c3d69c0a2839c481d425
izultatsumi <74152663+izultatsumi@users.noreply.github.com>
2020-11-09 02:17:09 +0700

    Add files via upload

---
+++ Assets/LineOfSightDemo/Scripts/enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class enemy : MonoBehaviour
+{
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Look();
+    }
+
+    
+
+    void Look()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            Vector3 lookDir = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+            Vector3 targetRotation = Quaternion.LookRotation(lookDir - transform.position).eulerAngles;
+            targetRotation = new Vector3(0, targetRotation.y, 0);
+
+            transform.rotation = Quaternion.Euler(targetRotation);
+        }
+    }
+}
